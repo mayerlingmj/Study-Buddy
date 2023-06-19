e1947ac1ac2276aef2c167f836955060f8c8d29c
Camille Marker <camillemarker@gmail.com>
2023-06-19 15:11:14 -0400

    Added 'Select School' href on index page, redirects to all schools page

--- studybuddy/main_app/urls.py
+++ studybuddy/main_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
     path('index', views.index, name='index'),
     path('study_groups/create/', views.CreateGroup.as_view(),
-         name='study_groups_create')
+         name='study_groups_create'),
+    path('main_app/school_select/', views.school_select, name='school_select')
 ]
