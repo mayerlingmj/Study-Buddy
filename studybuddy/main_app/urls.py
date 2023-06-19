14e8969a35e6b8b84d9b6d9ae5c75467c390c523
Camille Marker <camillemarker@gmail.com>
2023-06-18 20:58:02 -0400

    Create group functionality: coded the url path, class view Create Group, rendered template form

--- studybuddy/main_app/urls.py
+++ studybuddy/main_app/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
-  path('about/', views.about, name='about'),
-  path('accounts/signup/', views.signup, name='signup'),
-  path('index', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('accounts/signup/', views.signup, name='signup'),
+    path('index', views.index, name='index'),
+    path('study_groups/create/', views.CreateGroup.as_view(),
+         name='study_groups_create')
 ]
